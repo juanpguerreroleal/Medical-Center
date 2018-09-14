0060e0621b00146f3635a3a74b4e4618d9a2492e
Angel Cantu <arcantu97@gmail.com>
2018-09-14 00:48:16 -0500

    Ok

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { User } from '../../app/models/user';
 
 @IonicPage()
 @Component({
@@ -14,6 +8,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+
+  user = {} as User;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
