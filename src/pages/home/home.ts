4234492fc540f3e2d3e81f018e9399936a004df3
Angel Cantu <arcantu97@gmail.com>
2018-09-14 02:04:28 -0500

    Versión 1.0 de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from '@angular/fire/auth'
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private afAuth: AngularFireAuth, private toast: ToastController, public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  ionViewWillLoad(){
+    this.afAuth.authState.subscribe(data => {
+      if(data && data.email && data.uid) {
+        this.toast.create({
+          message: `Bienvenido otra vez, ${data.email}`,
+          duration: 3000
+
+      }).present();
+  }
+  else{
+    this.toast.create({
+      message: `Autenticación fallida`,
+      duration: 3000
+  }).present();
+  }
+    })
+  }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
+import { HomePage } from './../home/home';
+import { User } from './../../models/user';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { User } from '../../app/models/user';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @IonicPage()
 @Component({
@@ -9,13 +11,27 @@ import { User } from '../../app/models/user';
 })
 export class LoginPage {
 
-  user = {} as User;
+ user = {} as User;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  async login(user: User) {
+    try {
+      const result = this.afAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password);
+      if (result) {
+        this.navCtrl.push(HomePage);
+      }
+    }
+    catch (e) {
+      console.error(e);
+    }
+
   }
+//Falta por implementar
+  // MedicoLogin(){
+  //   this.navCtrl.push(HomePage);
+  // }
 
 }
